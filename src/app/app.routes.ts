466dc349c490cccac6f51636de59150aec764db4
jorgeSuarez-044 <jlsc4443@gmail.com>
2025-03-04 15:01:20 -0500

    creacion de rutas territorio inteligente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,13 @@
 import { Routes } from '@angular/router';
 import { MenuOpcionesComponent } from './views/medellin-menu/medellin-menu.component';
-import { InicioComponent } from './views/inicio/inicio.component';
+import { InicioComponent } from './views/inicio/inicio.component';  // Asegúrate de que el componente esté correctamente importado
 import { CamionesComponent } from './views/camiones/camiones.component';
 import { SensoresRuidoComponent } from './views/sensores-ruido/sensores-ruido.component';
 import { ReportesMedicos } from './views/reportes-medicos/reportesmed.component';
 import { PuntosWifi } from './views/puntos-wifi/puntoswifi.component';
 import { Vamosmed } from './views/vamosmedellin/vamosmed.component';
 import { Pqrsd } from './views/PQRSD/pqrsd.component';
+import { WhatsApp } from './views/whatsap/whatsap.component';
 import { Turismo } from './views/turismo/turismo.component';
 import { Siata } from './views/siata/siata.component';
 import { MapasCamiones } from './views/mapas-camiones/mapas-camiones.component';
@@ -18,21 +19,23 @@ import { TurismoMaps } from './views/turismomaps/turismomaps.component';
 import { SiataMaps } from './views/siatamaps/siatamaps.component';
 
 export const routes: Routes = [
-  { path: '', component: MenuOpcionesComponent },
-  { path: 'inicio', component: InicioComponent },
-  { path: 'ambiente/gestion-residuos/camiones', component: CamionesComponent },
-  { path: 'ambiente/calidad-ambiental/sensores-ruido', component: SensoresRuidoComponent },
-  { path: 'ambiente/calidad-ambiental/tablero-siata', component: Siata },
-  { path: 'gobernanza/reportesmed-tablero', component: ReportesMedicos },
-  { path: 'desarrollo-economico/transformacion-digital/tablero', component: PuntosWifi },
-  { path: 'movilidad-inteligente/vamosmed-tablero', component: Vamosmed },
-  { path: 'gobernanza/pqrs-tablero', component: Pqrsd },
-  { path: 'calidad-vida/turismo-tablero', component: Turismo },
-  { path: 'mapas/camiones', component: MapasCamiones },
-  { path: 'mapas/reportesmed', component: ReportesMedMap },  
-  { path: 'mapas/puntos-wifi', component: WifiMaps },  
-  { path: 'mapas/vamosmed', component: VamosMedMap }, 
-  { path: 'mapas/sensores-ruido', component: SensoresMap }, 
-  { path: 'mapas/turismo', component: TurismoMaps }, 
-  { path: 'mapas/siata', component: SiataMaps }
+  { path: '', component: MenuOpcionesComponent },  // Página principal con el menú
+  { path: 'inicio', component: InicioComponent },  // Componente al que se navega cuando se hace clic en "Camiones"
+  { path: 'territoriointeligente/visualizador/camiones', component: CamionesComponent },   // Componente al que se navega cuando se hace clic en "Reportes"
+  { path: 'territoriointeligente/visualizador/sensores-ruido', component: SensoresRuidoComponent },       // Reemplaza con el componente correspondiente
+  { path: 'territoriointeligente/visualizador/reportes', component: ReportesMedicos },   // Reemplaza con el componente correspondiente
+  { path: 'territoriointeligente/visualizador/puntos-wifi', component: PuntosWifi }, 
+  { path: 'territoriointeligente/visualizador/vamosmed', component: Vamosmed }, 
+  { path: 'territoriointeligente/visualizador/pqrsd', component: Pqrsd }, 
+  { path: 'territoriointeligente/visualizador/whatsapp', component: WhatsApp }, 
+  { path: 'territoriointeligente/visualizador/turismo', component: Turismo }, 
+  { path: 'territoriointeligente/visualizador/siata', component: Siata }, 
+  { path: 'territoriointeligente/visualizador/mapas/camiones', component: MapasCamiones },
+  { path: 'territoriointeligente/visualizador/mapas/reportes', component: ReportesMedMap },  
+  { path: 'territoriointeligente/visualizador/mapas/puntos-wifi', component: WifiMaps },  
+  { path: 'territoriointeligente/visualizador/mapas/vamosmed', component: VamosMedMap }, 
+  { path: 'territoriointeligente/visualizador/mapas/sensores-ruido', component: SensoresMap }, 
+  { path: 'territoriointeligente/visualizador/mapas/turismo', component: TurismoMaps }, 
+  { path: 'territoriointeligente/visualizador/mapas/siata', component: SiataMaps }, 
+
 ];
